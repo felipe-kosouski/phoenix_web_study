1082685d13d10fb47112598d1249e7c287b8d4d8
Felipe Kosouski <felipe.kosouski@gmail.com>
2020-09-28 23:46:01 -0300

    Finished Topics CRUD

--- lib/discuss/topic.ex
+++ lib/discuss/topic.ex
@@ -9,7 +9,7 @@ defmodule Discuss.Topic do
     timestamps()
   end
 
-  def changeset(%Topic{} = topic, attrs) do
+  def changeset(%Topic{} = topic, attrs \\ %{}) do
     topic
     |> cast(attrs, [:title])
     |> validate_required([:title])
